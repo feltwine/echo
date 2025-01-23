18cbc72a18cc79e19c9bd45ecf70e9762bedf28b
feltwine <patik.vaclav@proton.me>
2025-01-23 10:41:53 +0100

    Create default hubs migrations creating hubs table and moderators table

---
+++ database/migrations/2025_01_23_093322_create_hubs_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hubs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('name')->unique();
+            $table->text('description')->nullable();
+            $table->fullText(['name', 'description']);
+            $table->enum('status', ['public', 'private'])->default('private');
+
+            $table->string('avatar_path')->nullable();
+            $table->string('background_path')->nullable();
+            $table->string('background_color')->default('#FFFFFF');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::table('moderators', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('hub_id')->constrained('hubs')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hubs');
+    }
+};
