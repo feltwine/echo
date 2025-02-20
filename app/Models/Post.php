521b094a8c6b9016bb9e8a7443fc04005f1d7e49
feltwine <patik.vaclav@proton.me>
2025-02-20 11:58:41 +0100

    Import morphToMany relation to posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Carbon;
 
